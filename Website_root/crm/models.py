f3ea44cf982a1c3c7945f342ae50b52b6f78a6f2
Rustam0213 <rdvelihanov1245@gmail.com>
2024-12-03 17:59:43 +0100

    commit

--- Website_root/crm/models.py
+++ Website_root/crm/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 
